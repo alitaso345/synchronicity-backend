9bd25b2e8a4c9d33179a846f44a7a0e1b845a9dc
alitaso345 <alice.maru345@gmail.com>
2020-05-05 18:26:32 +0900

    init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module synchronicity-backend
+
+go 1.14
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
+	github.com/dghubble/oauth1 v0.6.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/thoj/go-ircevent v0.0.0-20190807115034-8e7ce4b5a1eb // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+
+	"github.com/kelseyhightower/envconfig"
+	irc "github.com/thoj/go-ircevent"
+)
+
+const serverssl = "irc.chat.twitch.tv:6697"
+
+type TwitchConfig struct {
+	Nick     string
+	Password string
+}
+
+type TwitterConfig struct {
+	ConsumerKey       string `envconfig:"CONSUMER_KEY"`
+	ConsumerSecret    string `envconfig:"CONSUMER_SECRET"`
+	AccessToken       string `envconfig:"ACCESS_TOKEN"`
+	AccessTokenSecret string `envconfig:"ACCESS_TOKEN_SECRET"`
+}
+
+func sse(w http.ResponseWriter, r *http.Request) {
+	flusher, _ := w.(http.Flusher)
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	go startTwitchCommentStream("#mogra", w, flusher)
+	go startTwitterHashTagStream("#mogra", w, flusher)
+
+	<-r.Context().Done()
+	log.Println("コネクションが閉じました")
+}
+
+func startTwitchCommentStream(channelName string, w http.ResponseWriter, flusher http.Flusher) {
+	var config TwitchConfig
+	envconfig.Process("TWITCH", &config)
+
+	nick := config.Nick
+	con := irc.IRC(nick, nick)
+
+	con.Password = config.Password
+	con.UseTLS = true
+	con.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	con.AddCallback("001", func(e *irc.Event) { con.Join(channelName) })
+	con.AddCallback("PRIVMSG", func(e *irc.Event) {
+		format := "data: {\"user\": \"%s\", \"text\": \"%s\", \"platform\": \"%s\"}\n\n"
+		fmt.Fprintf(w, format, e.User, e.Arguments[1], "twitch")
+		flusher.Flush()
+
+	})
+	err := con.Connect(serverssl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go con.Loop()
+}
+
+func startTwitterHashTagStream(hashTag string, w http.ResponseWriter, flusher http.Flusher) {
+	fmt.Println("started startTwitterHashTagStream")
+
+	var c TwitterConfig
+	envconfig.Process("TWITTER", &c)
+	config := oauth1.NewConfig(c.ConsumerKey, c.ConsumerSecret)
+	token := oauth1.NewToken(c.AccessToken, c.AccessTokenSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		format := "data: {\"user\": \"%s\", \"text\": \"%s\", \"platform\": \"%s\"}\n\n"
+		fmt.Fprintf(w, format, tweet.User.ScreenName, tweet.Text, "twitter")
+		flusher.Flush()
+	}
+
+	filterParams := &twitter.StreamFilterParams{Track: []string{hashTag}}
+	stream, err := client.Streams.Filter(filterParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go demux.HandleChan(stream.Messages)
+}
+
+func main() {
+	fmt.Println("start main")
+	http.HandleFunc("/events", sse)
+
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "5000"
+	}
+	http.ListenAndServe(":" + port, nil)
+}
