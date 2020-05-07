e411b94cc2cfe6187066af4fbbf150c429916c5e
alitaso345 <alice.maru345@gmail.com>
2020-05-08 00:34:07 +0900

    wip, reload doesn't work

--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
 	"github.com/kelseyhightower/envconfig"
 	irc "github.com/thoj/go-ircevent"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
 const serverssl = "irc.chat.twitch.tv:6697"
@@ -27,14 +24,8 @@ type TwitterConfig struct {
 	AccessTokenSecret string `envconfig:"ACCESS_TOKEN_SECRET"`
 }
 
-var twitchMessages chan *irc.Event = make(chan *irc.Event)
-var twitterMessages chan *twitter.Tweet = make(chan *twitter.Tweet)
-
 func main() {
 	log.Println("Start main...")
-
-	go startTwitchIrc("#aloonity")
-	go startTwitterStreaming("#ガルパピコ大盛り_01")
 
 	http.HandleFunc("/events", sse)
 
@@ -45,7 +36,7 @@ func main() {
 	http.ListenAndServe(":"+port, nil)
 }
 
-func startTwitchIrc(channelName string) {
+func startTwitchIrc(channelName string, twitchMessages chan<- *irc.Event, quit <-chan bool) {
 	if channelName == "" {
 		return
 	}
@@ -71,34 +62,38 @@ func startTwitchIrc(channelName string) {
 	}
 
 	con.Loop()
+
+	<-quit
+	con.Disconnect()
+	log.Println("IRCのコネクションを切断します")
 }
 
-func startTwitterStreaming(hashTag string) {
-	if hashTag == "" {
-		return
-	}
-
-	var c TwitterConfig
-	envconfig.Process("TWITTER", &c)
-	config := oauth1.NewConfig(c.ConsumerKey, c.ConsumerSecret)
-	token := oauth1.NewToken(c.AccessToken, c.AccessTokenSecret)
-	httpClient := config.Client(oauth1.NoContext, token)
-
-	client := twitter.NewClient(httpClient)
-
-	demux := twitter.NewSwitchDemux()
-	demux.Tweet = func(tweet *twitter.Tweet) {
-		twitterMessages <- tweet
-	}
-
-	filterParams := &twitter.StreamFilterParams{Track: []string{hashTag}}
-	stream, err := client.Streams.Filter(filterParams)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	demux.HandleChan(stream.Messages)
-}
+//func startTwitterStreaming(hashTag string) {
+//	if hashTag == "" {
+//		return
+//	}
+//
+//	var c TwitterConfig
+//	envconfig.Process("TWITTER", &c)
+//	config := oauth1.NewConfig(c.ConsumerKey, c.ConsumerSecret)
+//	token := oauth1.NewToken(c.AccessToken, c.AccessTokenSecret)
+//	httpClient := config.Client(oauth1.NoContext, token)
+//
+//	client := twitter.NewClient(httpClient)
+//
+//	demux := twitter.NewSwitchDemux()
+//	demux.Tweet = func(tweet *twitter.Tweet) {
+//		twitterMessages <- tweet
+//	}
+//
+//	filterParams := &twitter.StreamFilterParams{Track: []string{hashTag}}
+//	stream, err := client.Streams.Filter(filterParams)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	demux.HandleChan(stream.Messages)
+//}
 
 func sse(w http.ResponseWriter, r *http.Request) {
 	log.Println("Start sse...")
@@ -109,6 +104,13 @@ func sse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	quit := make(chan bool)
+
+	twitchMessages := make(chan *irc.Event)
+	//twitterMessages := make(chan *twitter.Tweet)
+
+	go startTwitchIrc("#aloonity", twitchMessages, quit)
+	//go startTwitterStreaming("#ガルパピコ大盛り_01")
+
 	flusher, _ := w.(http.Flusher)
 	format := "data: {\"user\": \"%s\", \"text\": \"%s\", \"platform\": \"%s\"}\n\n"
 	ctx := r.Context()
@@ -125,13 +127,13 @@ func sse(w http.ResponseWriter, r *http.Request) {
 					log.Println(msg.Arguments[1])
 					fmt.Fprintf(w, format, msg.User, msg.Arguments[1], "twitch")
 					flusher.Flush()
-				case msg := <-twitterMessages:
-					replacedMsg := strings.ReplaceAll(msg.Text, "\n", "")
-					replacedMsg = strings.ReplaceAll(replacedMsg, "\r", "")
-
-					log.Println(replacedMsg)
-					fmt.Fprintf(w, format, msg.User.ScreenName, replacedMsg, "twitter")
-					flusher.Flush()
+				//case msg := <-twitterMessages:
+				//	replacedMsg := strings.ReplaceAll(msg.Text, "\n", "")
+				//	replacedMsg = strings.ReplaceAll(replacedMsg, "\r", "")
+				//
+				//	log.Println(replacedMsg)
+				//	fmt.Fprintf(w, format, msg.User.ScreenName, replacedMsg, "twitter")
+				//	flusher.Flush()
 				}
 			}
 	}()
